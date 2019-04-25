4bd2324849918c88c6e9125866a74dd51ae72516
Serhii Filkovskyi <sfilkovskyi@oroinc.com>
2019-04-25 18:20:12 +0300

    add skeleton feature

---
+++ src/components/product_item.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProductItem = (props) => {
+    return (
+        <div></div>
+    )
+}
+
+export default ProductItem;
---
+++ src/containers/product_list.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProductList = React.createClass({
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+})
+
+export default ProductList;
